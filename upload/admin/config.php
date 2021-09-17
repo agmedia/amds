c86c6d5fa86cc106cda24808a0fa10c065ffb7ae
tomashina <tomislav@agmedia.hr>
2021-09-17 10:14:10 +0200

    config

--- upload/admin/config.php
+++ upload/admin/config.php
@@ -1,18 +1,18 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8888/amds/upload/admin/');
-define('HTTP_CATALOG', 'http://localhost:8888/amds/upload/');
+define('HTTP_SERVER', 'http://amds.selectpo.lin48.host25.com/admin/');
+define('HTTP_CATALOG', 'http://amds.selectpo.lin48.host25.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8888/amds/upload/admin/');
-define('HTTPS_CATALOG', 'http://localhost:8888/amds/upload/');
+define('HTTPS_SERVER', 'https://amds.selectpo.lin48.host25.com/admin/');
+define('HTTPS_CATALOG', 'https://amds.selectpo.lin48.host25.com/');
 
 // DIR
-define('DIR_APPLICATION', '/Applications/MAMP/htdocs/amds/upload/admin/');
-define('DIR_SYSTEM', '/Applications/MAMP/htdocs/amds/upload/system/');
-define('DIR_IMAGE', '/Applications/MAMP/htdocs/amds/upload/image/');
-define('DIR_STORAGE', '/Applications/MAMP/htdocs/amds/storage/');
-define('DIR_CATALOG', '/Applications/MAMP/htdocs/amds/upload/catalog/');
+define('DIR_APPLICATION', '/home/selectpo/amds.selectpo.lin48.host25.com/upload/admin/');
+define('DIR_SYSTEM', '/home/selectpo/amds.selectpo.lin48.host25.com/upload/system/');
+define('DIR_IMAGE', '/home/selectpo/amds.selectpo.lin48.host25.coms/upload/image/');
+define('DIR_STORAGE', '/home/selectpo/amds.selectpo.lin48.host25.com/storage/');
+define('DIR_CATALOG', '/home/selectpo/amds.selectpo.lin48.host25.com/upload/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -26,9 +26,9 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'bakanal');
-define('DB_DATABASE', 'amds');
+define('DB_USERNAME', 'selectpo_amds');
+define('DB_PASSWORD', 'v1^,62jQi0Qa');
+define('DB_DATABASE', 'selectpo_amds');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
--- upload/config.php
+++ upload/config.php
@@ -1,15 +1,15 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8888/amds/upload/');
+define('HTTP_SERVER', 'http://amds.selectpo.lin48.host25.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8888/amds/upload/');
+define('HTTPS_SERVER', 'https://amds.selectpo.lin48.host25.com/');
 
 // DIR
-define('DIR_APPLICATION', '/Applications/MAMP/htdocs/amds/upload/catalog/');
-define('DIR_SYSTEM', '/Applications/MAMP/htdocs/amds/upload/system/');
-define('DIR_IMAGE', '/Applications/MAMP/htdocs/amds/upload/image/');
-define('DIR_STORAGE', '/Applications/MAMP/htdocs/amds/storage/');
+define('DIR_APPLICATION', '/home/selectpo/amds.selectpo.lin48.host25.com/upload/catalog/');
+define('DIR_SYSTEM', '/home/selectpo/amds.selectpo.lin48.host25.com/upload/system/');
+define('DIR_IMAGE', '/home/selectpo/amds.selectpo.lin48.host25.com/upload/image/');
+define('DIR_STORAGE', '/home/selectpo/amds.selectpo.lin48.host25.com/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -23,8 +23,8 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'bakanal');
-define('DB_DATABASE', 'amds');
+define('DB_USERNAME', 'selectpo_amds');
+define('DB_PASSWORD', 'v1^,62jQi0Qa');
+define('DB_DATABASE', 'selectpo_amds');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
