511f6c48221478155137238fd7129fa5b62ddd8a
tomashina <tomislav@agmedia.hr>
2021-11-05 14:01:48 +0100

    product css

--- upload/catalog/controller/product/category.php
+++ upload/catalog/controller/product/category.php
@@ -200,18 +200,15 @@ class ControllerProductCategory extends Controller {
 
                     foreach ($option['product_option_value'] as $option_value) {
                         if (!$option_value['subtract'] || ($option_value['quantity'] > 0)) {
-                            if ((($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) && (float)$option_value['price']) {
-                                $price = $this->currency->format($this->tax->calculate($option_value['price'], $product_info['tax_class_id'], $this->config->get('config_tax') ? 'P' : false), $this->session->data['currency']);
-                            } else {
-                                $price = false;
-                            }
+
+
 
                             $product_option_value_data[] = array(
                                 'product_option_value_id' => $option_value['product_option_value_id'],
                                 'option_value_id'         => $option_value['option_value_id'],
                                 'name'                    => $option_value['name'],
                                 'image'                   => $this->model_tool_image->resize($option_value['image'], 50, 50),
-                                'price'                   => $price,
+
                                 'price_prefix'            => $option_value['price_prefix'],
                                 'sku'            => $option_value['sku']
                             );
