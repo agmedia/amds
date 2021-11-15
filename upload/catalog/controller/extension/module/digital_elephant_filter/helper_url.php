2d8f97f738ff9c93c35ee5daae3414de86f4fe61
tomashina <tomislav@agmedia.hr>
2021-11-15 18:07:42 +0100

    order

--- upload/catalog/controller/extension/module/digital_elephant_filter/helper_url.php
+++ upload/catalog/controller/extension/module/digital_elephant_filter/helper_url.php
@@ -83,13 +83,13 @@ class ControllerExtensionModuleDigitalElephantFilterHelperUrl extends Controller
             if (isset($this->request->get['sort'])) {
                 $sort = $this->request->get['sort'];
             } else {
-                $sort = 'p.date_added-DESC';
+                $sort = 'p.date_added';
             }
 
             if (isset($this->request->get['order'])) {
                 $order = $this->request->get['order'];
             } else {
-                $order = 'ASC';
+                $order = 'DESC';
             }
 
             if (isset($this->request->get['page'])) {
