cefab3aebd8ed81c6397ceafbd2fbb6d507de641
tomashina <tomislav@agmedia.hr>
2021-12-08 09:59:58 +0100

    success

--- upload/catalog/controller/checkout/success.php
+++ upload/catalog/controller/checkout/success.php
@@ -16,7 +16,7 @@ class ControllerCheckoutSuccess extends Controller {
 			unset($this->session->data['payment_methods']);
 			unset($this->session->data['guest']);
 			unset($this->session->data['comment']);
-			unset($this->session->data['order_id']);
+
 			unset($this->session->data['coupon']);
 			unset($this->session->data['reward']);
 			unset($this->session->data['voucher']);
@@ -87,7 +87,7 @@ class ControllerCheckoutSuccess extends Controller {
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
-
+        unset($this->session->data['order_id']);
 		$this->response->setOutput($this->load->view('common/success', $data));
 	}
 }
