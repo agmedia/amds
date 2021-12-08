1aa9b08cc5642fe524a11c7b4f961bbc61479b5a
tomashina <tomislav@agmedia.hr>
2021-12-08 18:31:41 +0100

    option quantity

--- upload/catalog/controller/product/product.php
+++ upload/catalog/controller/product/product.php
@@ -312,7 +312,7 @@ class ControllerProductProduct extends Controller {
 				$product_option_value_data = array();
 
 				foreach ($option['product_option_value'] as $option_value) {
-					if (!$option_value['subtract'] || ($option_value['quantity'] > 0)) {
+					if ($option_value['quantity'] > 0) {
 						if ((($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) && (float)$option_value['price']) {
 							$price = $this->currency->format($this->tax->calculate($option_value['price'], $product_info['tax_class_id'], $this->config->get('config_tax') ? 'P' : false), $this->session->data['currency']);
 						} else {
