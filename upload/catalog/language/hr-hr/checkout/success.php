731f43e10e282c13afba85a30a89ca3e7aec58ad
tomashina <tomislav@agmedia.hr>
2021-12-21 19:28:33 +0100

    dashboard

--- upload/catalog/language/hr-hr/checkout/success.php
+++ upload/catalog/language/hr-hr/checkout/success.php
@@ -1,7 +1,7 @@
 <?php
 // Croatian   v.2.x.x     Datum: 01.10.2014		Author: Gigo (Igor Ilić - igor@iligsoft.hr)
 // Heading
-$_['heading_title']        = 'Hvala vam na povjerenju!';
+$_['heading_title']        = 'Narudžbe izvršena';
 
 // Text
 $_['text_basket']          = 'Košarica';
