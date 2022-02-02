72ec5e44dda0584ff5bcc16029abe515add96be9
tomashina <tomislav@agmedia.hr>
2022-02-02 13:56:49 +0100

    order admin

--- upload/admin/controller/common/column_left.php
+++ upload/admin/controller/common/column_left.php
@@ -237,13 +237,13 @@ class ControllerCommonColumnLeft extends Controller {
 			// Sales
 			$sale = array();
 
-			if ($this->user->hasPermission('access', 'sale/order')) {
+		/*	if ($this->user->hasPermission('access', 'sale/order')) {
 				$sale[] = array(
 					'name'	   => $this->language->get('text_order'),
 					'href'     => $this->url->link('sale/order', 'user_token=' . $this->session->data['user_token'], true),
 					'children' => array()
 				);
-			}
+			}*/
 
 			if ($this->user->hasPermission('access', 'sale/recurring')) {
 				$sale[] = array(
