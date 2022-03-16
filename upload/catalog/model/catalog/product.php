41ed391f9aeb458be138cc8f26cbb5d972d64fe0
tomashina <tomislav@agmedia.hr>
2022-03-16 15:21:13 +0100

    cart prosucts

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -435,7 +435,7 @@ class ModelCatalogProduct extends Model {
 
 
         $getCat = $this->db->query("SELECT product_id, min(category_id) as category_id FROM " . DB_PREFIX . "product_to_category WHERE product_id = '" . (int)$product_id . "' AND category_id not in (SELECT distinct parent_id as category_id FROM oc_category)");
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_to_category p2c LEFT JOIN " . DB_PREFIX . "product p ON (p2c.product_id = p.product_id) WHERE (p2c.category_id = '" . (int)$getCat->row['category_id'] . "' AND (p2c.product_id != '" . (int)$product_id . "') AND p.status = '1'  LIMIT 0,5");
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_to_category p2c LEFT JOIN " . DB_PREFIX . "product p ON (p2c.product_id = p.product_id) WHERE (p2c.category_id = '" . (int)$getCat->row['category_id'] . "' AND p.status = '1'  AND (p2c.product_id != '" . (int)$product_id . "'  LIMIT 0,5");
 
         foreach ($query->rows as $result) {
             //$product_data[$result['related_id']] = $this->getProduct($result['related_id']);
