643bf2bcf7443941ccba968f6c4cf0898796114e
tomashina <tomislav@agmedia.hr>
2022-03-30 08:33:16 +0200

    facebook

--- upload/catalog/controller/extension/feed/facebookstore.php
+++ upload/catalog/controller/extension/feed/facebookstore.php
@@ -22,7 +22,7 @@ class ControllerExtensionFeedFacebookstore extends Controller {
 
         foreach ($products as $product) {
 
-            if($product['quantity'] > 0 || $product['model']!='') {
+            if($product['quantity'] > 0 && $product['model']!='') {
 
 
 
