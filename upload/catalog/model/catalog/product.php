aa3de0b54f960e7083d0bb8d0f579f37c37f489c
tomashina <tomislav@agmedia.hr>
2023-03-28 07:12:22 +0200

    popust fix

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -25,6 +25,7 @@ class ModelCatalogProduct extends Model {
 				'mpn'              => $query->row['mpn'],
 				'location'         => $query->row['location'],
 				'quantity'         => $query->row['quantity'],
+                'imported'         => $query->row['imported'],
 				'stock_status'     => $query->row['stock_status'],
 				'image'            => $query->row['image'],
 				'manufacturer_id'  => $query->row['manufacturer_id'],
