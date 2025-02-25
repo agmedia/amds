29a73f977a02db9ffa4997829f1a624f3cf8c8b9
tomashina <tomislav@agmedia.hr>
2025-02-25 11:07:59 +0100

    dpd

--- upload/catalog/language/hr-hr/extension/shipping/free.php
+++ upload/catalog/language/hr-hr/extension/shipping/free.php
@@ -1,5 +1,5 @@
 <?php
 // Croatian   v.2.x.x     Datum: 01.10.2014		Author: Gigo (Igor Ilić - igor@iligsoft.hr)
 // Text
-$_['text_title']       = 'IN TIME - Besplatna dostava';
-$_['text_description'] = 'IN TIME - Besplatna dostava';
+$_['text_title']       = 'DPD - Besplatna dostava';
+$_['text_description'] = 'DPD - Besplatna dostava';
