f3bbad01031636f0785056e1eff9b9e4cbd0006a
tomashina <tomislav@agmedia.hr>
2026-03-02 11:56:44 +0100

    fix order

--- upload/admin/controller/extension/module/me_order_manager.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once DIR_APPLICATION . 'controller/extension/me_order_manager.php';
-
-class ControllerExtensionModuleMeordermanager extends ControllerExtensionMeordermanager {}
--- upload/admin/controller/extension/module/me_order_manager_setting.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once DIR_APPLICATION . 'controller/extension/me_order_manager_setting.php';
-
-class ControllerExtensionModuleMeordermanagersetting extends ControllerExtensionMeordermanagersetting {}
