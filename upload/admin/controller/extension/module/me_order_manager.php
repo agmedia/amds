c69a889626d1c3ab4902421402c8e4f5bce99fab
tomashina <tomislav@agmedia.hr>
2026-03-02 10:47:16 +0100

    fix order

---
+++ upload/admin/controller/extension/module/me_order_manager.php
@@ -0,0 +1,4 @@
+<?php
+require_once DIR_APPLICATION . 'controller/extension/me_order_manager.php';
+
+class ControllerExtensionModuleMeordermanager extends ControllerExtensionMeordermanager {}
---
+++ upload/admin/controller/extension/module/me_order_manager_setting.php
@@ -0,0 +1,4 @@
+<?php
+require_once DIR_APPLICATION . 'controller/extension/me_order_manager_setting.php';
+
+class ControllerExtensionModuleMeordermanagersetting extends ControllerExtensionMeordermanagersetting {}
